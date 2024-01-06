ce72790576018247b669f6ab3ee938ce4098654b
BMES89 <bessyescobar89@gmail.com>
2024-01-06 19:21:43 +0100

    delete favorite attempt 1

--- src/js/main.js
+++ src/js/main.js
@@ -31,11 +31,11 @@ function renderOne(characterData) {
   </li>`;
 };
 
-function renderAll() {
+function renderAll(data) {
     characterList.innerHTML = '';
 
-    for( let i=0; i < filteredData.length; i++ ) {
-      renderOne( filteredData[i] );
+    for( let i=0; i < data.length; i++ ) {
+      renderOne( data[i] );
     };
 
     const allCharactersLi = document.querySelectorAll('.js_charactersDisney');
@@ -50,6 +50,7 @@ function renderOneFavorite(favoritesData) {
     favoritesList.innerHTML += `
     <li class="js_charactersDisney" id="${favoritesData._id}">
     <div class="">
+    <div class="js_favoriteCharacterClosed closed">x</div>
     <h3 class="" id="${favoritesData._id}">${favoritesData.name}</h3>
     <img class="" src="${favoritesData.imageUrl}" alt="character ${favoritesData.name}" title="character ${favoritesData.name}"/> 
     </div>
@@ -78,10 +79,21 @@ console.log(indexCharacter)
     }
 
     renderFavorite();
-    // clickedList.classList.toggle('favorites')
+    // clickedList.classList.toggle('favorite')// no funciona
 
     console.log('funciona')
 };
+
+function handleClickRemoveFavorite (event) {
+    const idCharacter = parseInt(event.currentTarget.id);
+
+    console.log(idCharacter);
+    const favoriteCharacterIndex = favoritesData.findIndex( (favorite) => favorite._id === idCharacter );
+
+    favoritesData.splice(favoriteCharacterIndex, 1);
+    favoritesData.classList.toggle('favorites')
+    renderFavorite(favoritesData);
+};//no funciona
 
 //Eventos
 formSearch.addEventListener( 'submit' , (event) => {
@@ -94,48 +106,26 @@ formSearch.addEventListener( 'submit' , (event) => {
       .then(data => {
         disneyData = data.data;
 
-        renderAll()
+        renderAll(filteredData)
       })
 
-      console.log(formSearch)
 });
 
 
 
-// const handleClickSearch = (event) => {
-//     event.prevenDefault();
-//     const searchValue = inputSearch.value;  
-//     const filterList = disneyData.filter(character) => character.name.toLowerCase().includes(searchValue.toLowerCase())
 
-//     renderAll()
-// }
-
-// buttonSearch.addEventListener('Click' , handleClickSearch);
-
-
-
-// buttonSearch.addEventListener( 'submit' , (event) => {
-//     event.preventDefault();
-
-//     disneyData = disneyData.filter((character) => character.name.toLowerCase().incluedes( inputSearch.value.toLowerCase() ));
-
-//     renderAll();
-
-
-
-// });
 
 
 //Código cuando carga la Pág
 
-renderAll()
+renderAll(disneyData)
 
 fetch('//api.disneyapi.dev/character?pageSize=50')
   .then( response => response.json() )
   .then( data => {
   disneyData = data.data;
 
-  renderAll();
+  renderAll(disneyData);
   });
 
 
@@ -159,3 +149,29 @@ fetch('//api.disneyapi.dev/character?pageSize=50')
 //     <img class="" src="${characterData.imageUrl}" alt="character ${characterData.name}" title="character ${characterData.name}"/> 
 //     </div>
 //   </li>`;
+
+
+
+//codigo para busqueda
+// const handleClickSearch = (event) => {
+//     event.prevenDefault();
+//     const searchValue = inputSearch.value;  
+//     const filterList = disneyData.filter(character) => character.name.toLowerCase().includes(searchValue.toLowerCase())
+
+//     renderAll()
+// }
+
+// buttonSearch.addEventListener('Click' , handleClickSearch);
+
+
+
+// buttonSearch.addEventListener( 'submit' , (event) => {
+//     event.preventDefault();
+
+//     disneyData = disneyData.filter((character) => character.name.toLowerCase().incluedes( inputSearch.value.toLowerCase() ));
+
+//     renderAll();
+
+
+
+// });
